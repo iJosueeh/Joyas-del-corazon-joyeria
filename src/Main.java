cf0a6d99c9c2a34cbe21be4b7ef46e151f9f8ee6
HOME <HOME@DESKTOP-1MP425B>
2024-11-09 10:27:02 -0500

    Inicializando proyecto final

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import vistas.menu.MenuInicial;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HOME
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Inicializar el menu inicial
+        MenuInicial menuInicial = new MenuInicial();
+        menuInicial.setVisible(true);
+        menuInicial.setLocationRelativeTo(null);
+    }
+}
