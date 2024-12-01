c99b8703a582095f35542b70c9ebf97201b806a3
iJosueeh <royer.tanta27@gmail.com>
2024-12-01 12:37:17 -0500

    Actualizando productos

---
+++ src/modelos/clases/productos/Proveedor.java
@@ -0,0 +1,17 @@
+
+package modelos.clases.productos;
+
+public class Proveedor {
+    private int idProveedor;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Proveedor(int idProveedor, String nombre, String telefono, String direccion) {
+        this.idProveedor = idProveedor;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+  
+}
