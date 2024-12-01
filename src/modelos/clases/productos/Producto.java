c1abced48ec29184cb23a44f5a03690439c555f6
iJosueeh <royer.tanta27@gmail.com>
2024-12-01 12:37:02 -0500

    Actualizando productos

---
+++ src/modelos/clases/productos/Producto.java
@@ -0,0 +1,82 @@
+package modelos.clases.productos;
+
+import java.util.List;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int cantidad;
+    private int stock;
+    private Colecciones coleccion;
+
+    public Producto(String nombre, String descripcion, double precio, int cantidad, int stock, Colecciones coleccion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.stock = stock;
+        this.coleccion = coleccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Colecciones getColeccion() {
+        return coleccion;
+    }
+
+    public void setColeccion(Colecciones coleccion) {
+        this.coleccion = coleccion;
+    }
+
+    
+    
+}
